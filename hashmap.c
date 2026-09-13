d68135739d2afc20907d8dd35b1b91f4da4ef9fe
Your Name <you@example.com>
2026-09-13 23:18:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -49,7 +49,7 @@ HashMap * createMap(long capacity) {
     map->capacity = capacity;
     map->size = 0;
     map->current = -1;
-
+    map->buckets = (Pair **)calloc(capacity, sizeof(Pair *));
     return map;
 }
 
